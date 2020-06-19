ea28c3ea75469c448bc742dcf5703484a56639f7
Rafi Aroch <rafi.aroch@gmail.com>
2020-06-20 01:15:06 +0300

    add answer

--- src/main/scala/com/aroch/spark/Main.scala
+++ src/main/scala/com/aroch/spark/Main.scala
@@ -1,7 +1,9 @@
 package com.aroch.spark
 
 import org.apache.log4j.Logger
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{LongType, StringType, StructType, TimestampType}
+import org.apache.spark.sql.{SaveMode, SparkSession}
 
 object Main {
 
@@ -11,8 +13,49 @@ object Main {
 
     val spark: SparkSession = SparkSession.builder.appName("Interview").master("local[*]").getOrCreate()
 
-    import spark.implicits._
+    val schema1 = new StructType()
+      .add("time", TimestampType)
+      .add("type", StringType)
+      .add("uid", StringType)
+      .add("country", StringType)
 
+    val schema2 = new StructType()
+      .add("time", LongType)
+      .add("type", StringType)
+      .add("uid", StringType)
+      .add("country", StringType)
+
+    val source1 = spark.read
+      .option("header", "true")
+      .option("timestampFormat", "dd MMM yyyy HH:mm:ss z")
+      .schema(schema1)
+      .csv("/Users/aroch/git/spark-scala-interview/src/main/resources/source_1/*/*/*/*")
+
+    val source2 = spark.read
+      .option("header", "true")
+      .schema(schema2)
+      .csv("/Users/aroch/git/spark-scala-interview/src/main/resources/source_2/*/*/*/*")
+
+    val union = source2.select(
+      col("time").cast(TimestampType),
+      col("type"),
+      col("uid"),
+      col("country")
+    )
+      .union(source1)
+      .withColumn("year", year(col("time")))
+      .withColumn("month", month(col("time")))
+      .withColumn("day", dayofmonth(col("time")))
+      .orderBy(desc("time"))
+      .coalesce(1)
+
+    union
+      .write
+      .mode(SaveMode.Overwrite)
+      .partitionBy("year", "month", "day")
+      .csv("output")
+
+    spark.sparkContext.stop()
   }
 
 }
