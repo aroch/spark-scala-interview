34cf73be3c8ee2bdca4f033502cbc6c7e0dcd163
Rafi Aroch <rafi.aroch@gmail.com>
2020-06-20 23:25:04 +0300

    moving ex1

--- src/main/scala/com/aroch/spark/ex1/ProcessingTimeToEventTime.scala
+++ src/main/scala/com/aroch/spark/ex1/ProcessingTimeToEventTime.scala
@@ -1,17 +1,15 @@
-package com.aroch.spark
+package com.aroch.spark.ex1
 
 import org.apache.log4j.Logger
 import org.apache.spark.sql.SparkSession
 
-object Main {
+object ProcessingTimeToEventTime {
 
   @transient lazy val logger: Logger = Logger.getLogger(getClass.getName)
 
   def main(args: Array[String]): Unit = {
 
     val spark: SparkSession = SparkSession.builder.appName("Interview").master("local[*]").getOrCreate()
-
-    import spark.implicits._
 
   }
 
